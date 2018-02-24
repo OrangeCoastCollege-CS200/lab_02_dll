04fa9be996ed6e47ead394c100211ed61995e567
Severiano Badajoz <sevebadajoz@gmail.com>
2018-02-24 14:58:14 -0800

    Fix and check efficency of switchEveryTwo

--- Project1/Functions.cpp
+++ Project1/Functions.cpp
@@ -54,36 +54,33 @@ DoublyList& DoublyList::operator=(DoublyList&& other) {
 
 // Definition switchEveryTwo
 void DoublyList::switchEveryTwo() {
-	int newCount = count;
-	Node* curr = first;
-	Node* temp;
-		if (count % 2 != 0) newCount -= 1;
-		for (int i = 0; i < newCount; i += 2, curr = curr->getNext()) {
-			temp = new Node(*curr);
-
-			curr->setPrev(curr->getNext());
-			curr->setNext(curr->getNext()->getNext());
-
-			curr->getPrev()->setNext(curr);
-
-			if (i == 0)
-				first = temp->getNext();
-			else
-				curr->getPrev()->setPrev(temp->getPrev());
+	if (count < 2)
+		cout << "List is too small (Must have at least two nodes)";
+	else {
+		Node * temp = first;
+		Node * curr = first->getNext();
+		while (temp != nullptr && curr != nullptr) {
+			curr->setPrev(temp->getPrev());
+			if (curr->getPrev() != nullptr) curr->getPrev()->setNext(curr);
+			temp->setPrev(curr);
+			temp->setNext(curr->getNext());
+			curr->setNext(temp);
+			if (temp->getNext() != nullptr) temp->getNext()->setPrev(temp);
+			temp = temp->getNext();
+			if (temp != nullptr) curr = temp->getNext();
 		}
-		
-		first->setPrev(nullptr);
-		
-		if (newCount != count) {
-			Node* temp = first;
-			first = last;
-			first->setNext(last->getNext());
-			last = temp;
-			last->setPrev(first->getPrev());
+		if (count % 2 == 0) {
+			last = last->getNext();
+			first = first->getPrev();
+		}
+		else {
+			last = last->getPrev();
 			last->setNext(nullptr);
-			first->setPrev(nullptr);
-			
-		} else {
-			last = curr;
+			temp->setPrev(nullptr);
+			temp->setNext(first->getNext);
+			first->getPrev()->setPrev(temp);
+			first = temp;
+		}
 	}
 }
+
